35eefd57f8e1d05eae3097b3c28a68be5d6a3cd2
Nazmul177 <nazmul177382@gmail.com>
2020-07-19 00:56:20 +0600

    true false

---
+++ true-false.js
@@ -0,0 +1,16 @@
+// False :
+// 0, -0
+//null
+//false,undefined, NaN, ""
+
+//True :
+// [], " ", "false"
+
+let name = NaN;
+console.log(name);
+if(name){
+    console.log("Condition is true");
+}
+else{
+    console.log("Condition is false");
+}
