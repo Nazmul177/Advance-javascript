ecca4e8c92dd77b1d603e0f10160c1105a7f0870
Nazmul177 <nazmul177382@gmail.com>
2020-07-19 01:27:11 +0600

    double  vs tripple equal

---
+++ null-vs-undefined.js
@@ -0,0 +1,18 @@
+let pakhi;
+function add(num1, num2){
+    console.log(num1+num2);
+    return
+}
+function add(num1, num2){
+    console.log(num1, num2);
+    
+}
+const result = add(12);
+console.log(result);
+
+const premik = {name:"smart dude", phone:345432};
+console.log(premik.gf);
+let ages = [2, 3, 4]
+console.log([11]);
+
+null
